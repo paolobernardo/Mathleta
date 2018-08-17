11fd4358c28dcbd9d4ba82b8bcb2529ed48c6162
Paolo Bernardo <PaoloB@paolos-macbook-pro.local>
2018-08-18 11:18:37 +1200

    Changed answer shuffle function

--- Mathleta/Model/Equation.swift
+++ Mathleta/Model/Equation.swift
@@ -85,7 +85,7 @@ class Equation {
         }
         
         randomAnswers.append(correctAnswer)
-        randomAnswers.shuffle()
+        shuffleAnswer()
         
     }
     
@@ -101,22 +101,18 @@ class Equation {
     }
     
     
-}
-
-
-//Used to shuffle the list of random answers to be used in the button labels
-//Obtained from https://stackoverflow.com/questions/24026510/how-do-i-shuffle-an-array-in-swift
-extension MutableCollection {
-    /// Shuffles the contents of this collection.
-    mutating func shuffle() {
-        let c = count
-        guard c > 1 else { return }
+    func shuffleAnswer(){
+        var tempAnswers = [Int]()
         
-        for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
-            // Change `Int` in the next line to `IndexDistance` in < Swift 4.1
-            let d: Int = numericCast(arc4random_uniform(numericCast(unshuffledCount)))
-            let i = index(firstUnshuffled, offsetBy: d)
-            swapAt(firstUnshuffled, i)
+        for _ in 0..<randomAnswers.count{
+            let randomNumber = Int(arc4random_uniform(UInt32(randomAnswers.count)))
+            tempAnswers.append(randomAnswers[randomNumber])
+            randomAnswers.remove(at: randomNumber)
         }
+        randomAnswers = tempAnswers
     }
+    
+    
 }
+
+
